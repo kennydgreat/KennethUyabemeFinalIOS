c772b700d28e99fc7900ee2d954702ea728839fc
Kenneth Uyabeme <uyabeme@oa-s231-11.ddi.sheridanc.on.ca>
2018-12-14 11:07:33 -0500

    Initial Commit

---
+++ KennethUyabemeFinalIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KennethUyabemeFinalIOS
+//
+//  Created by Kenneth Uyabeme on 12/14/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
